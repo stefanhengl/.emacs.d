8ed50f88bc497c4b5ec960b2a8140e0c7c508c74
stefan hengl <stefan.hengl@zalando.de>
2019-04-18 11:43:14 +0200

    beautify org-mode

--- init.el
+++ init.el
@@ -28,5 +28,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-document-title ((t (:inherit default :weight bold :foreground "#eceff4" :font "Helvetica" :height 2.0 :underline nil))))
+ '(org-level-1 ((t (:inherit default :weight bold :foreground "#eceff4" :font "Helvetica" :height 1.3))))
+ '(org-level-2 ((t (:inherit default :weight bold :foreground "#eceff4" :font "Helvetica" :height 1.2))))
+ '(org-level-3 ((t (:inherit default :weight bold :foreground "#eceff4" :font "Helvetica" :height 1.1))))
+ '(org-level-4 ((t (:inherit default :weight bold :foreground "#eceff4" :font "Helvetica"))))
+ '(org-level-5 ((t (:inherit default :weight bold :foreground "#eceff4" :font "Helvetica"))))
+ '(org-level-6 ((t (:inherit default :weight bold :foreground "#eceff4" :font "Helvetica"))))
+ '(org-level-7 ((t (:inherit default :weight bold :foreground "#eceff4" :font "Helvetica"))))
+ '(org-level-8 ((t (:inherit default :weight bold :foreground "#eceff4" :font "Helvetica"))))
  '(region ((t (:background "Purple")))))
 
