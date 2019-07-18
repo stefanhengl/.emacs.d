05e7cc62ca37794ce98d9afb3a707eb4fe5b064f
stefan hengl <stefan.hengl@zalando.de>
2019-07-18 17:38:31 +0200

    adding hydra and flycheck

--- init.el
+++ init.el
@@ -23,9 +23,14 @@
    (quote
     ("fe666e5ac37c2dfcf80074e88b9252c71a22b6f5d2f566df9a7aa4f9bea55ef8" "a3fa4abaf08cc169b61dea8f6df1bbe4123ec1d2afeb01c17e11fdc31fc66379" "fa2af0c40576f3bde32290d7f4e7aa865eb6bf7ebe31eb9e37c32aa6f4ae8d10" "d2e9c7e31e574bf38f4b0fb927aaff20c1e5f92f72001102758005e53d77b8c9" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "10461a3c8ca61c52dfbbdedd974319b7f7fd720b091996481c8fb1dded6c6116" "170bb47b35baa3d2439f0fd26b49f4278e9a8decf611aa33a0dad1397620ddc3" default)))
  '(markdown-command "/usr/local/bin/pandoc")
+ '(org-agenda-files (quote ("~/.emacs.d/config.org")))
  '(package-selected-packages
    (quote
-    (dap-go ag spacemacs-theme auto-complete auto-complete-config iedit exec-path-from-shell evil-magit magit diff-hl avy which-key general evil-leader clj-refactor cider evil-surround json-mode yaml-mode all-the-icons-ivy neotree git-gutter-fringe git-gutter doom-themes use-package smartparens rainbow-mode rainbow-delimiters projectile paredit markdown-mode highlight-parentheses god-mode evil ac-nrepl))))
+    (dap-go ag spacemacs-theme auto-complete auto-complete-config iedit exec-path-from-shell evil-magit magit diff-hl avy which-key general evil-leader clj-refactor cider evil-surround json-mode yaml-mode all-the-icons-ivy neotree git-gutter-fringe git-gutter doom-themes use-package smartparens rainbow-mode rainbow-delimiters projectile paredit markdown-mode highlight-parentheses god-mode evil ac-nrepl)))
+ '(safe-local-variable-values
+   (quote
+    ((cider-ns-refresh-before-fn . "user/stop")
+     (cider-ns-refresh-after-fn . "user/start")))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
